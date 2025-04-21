82b7ab50703eee9413f5c86e00abd38d28fea0ff
nanegit <filesnane@gmail.com>
2025-04-22 00:04:10 +0400

    Add files via upload

---
+++ password_entry.h
@@ -0,0 +1,13 @@
+#ifndef PASSWORD_ENTRY_H
+#define PASSWORD_ENTRY_H
+
+#include <string>
+
+struct PasswordEntry {
+    std::string service;
+    std::string username;
+    std::string password;
+};
+
+#endif
+#pragma once
